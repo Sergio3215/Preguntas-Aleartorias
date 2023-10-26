9af759d53f8a360f4d50da859e3c28f5f2a77b95
Sergio <principiante.en.programar@gmail.com>
2023-10-26 13:53:20 -0300

    change responsive design and some fix about style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 // tailwind.config.js
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -12,6 +12,22 @@ module.exports = {
     extend: {
       colors: {
         'regal-blue': '#243c5a',
+      },
+      screens: {
+        'smb': '140px',
+        // => @media (min-width: 140px) { ... }
+        'mb': '240px',
+        // => @media (min-width: 240px) { ... }
+        'lmb': '454px',
+        // => @media (min-width: 454px) { ... }
+        'sm': '684px',
+        // => @media (min-width: 684px) { ... }
+
+        'md': '960px',
+        // => @media (min-width: 960px) { ... }
+
+        'lg': '1440px',
+        // => @media (min-width: 1440px) { ... }
       },
     }
   },
