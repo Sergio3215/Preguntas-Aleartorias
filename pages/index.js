fb47004b744cb6c1ee5232a8c5fe655bb105c1f0
Sergio <principiante.en.programar@gmail.com>
2023-10-26 15:47:27 -0300

    add blur when open modal

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function App() {
 
     const [question, setQuestion] = useState([]);
     const [answer, setAnswer] = useState(false);
+    const [blur, setBlur] = useState("");
 
     useEffect(() => {
         document.querySelector('body').style.height = innerHeight + "px";
@@ -30,16 +31,16 @@ export default function App() {
 
     return (
         <div>
-            <Card className='w-2/3 lg:left-72 md:left-40 top-unit-60 h-2/3 sm:left-36 lmb:left-28 mb:left-12'>
+            <Card className={`w-2/3 lg:left-72 md:left-40 top-unit-60 h-2/3 sm:left-36 lmb:left-28 mb:left-12 ${blur}`}>
                 <CardBody>
                     <div className='text-center font-bold lg:text-8xl sm:text-5xl lmb:text-4xl mb:text-2xl'>
                         Preguntas Aleatorias
                         {/* <Divider /> */}
-                        <Button className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 lg:w-unit-7xl md:w-unit-7xl sm:w-unit-7xl h-unit-3xl lg:text-5xl sm:text-5xl left-5 lmb:left-2 lmb:text-5xl  lmb:w-unit-7x mb:left-1 mb:text-2xl" onPress={async () => { extractJSON(); onOpen(); }}>Comenzar</Button>
+                        <Button className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 lg:w-unit-7xl md:w-unit-7xl sm:w-unit-7xl h-unit-3xl lg:text-5xl sm:text-5xl left-5 lmb:left-2 lmb:text-5xl  lmb:w-unit-7x mb:left-1 mb:text-2xl" onPress={async () => { extractJSON(); onOpen(); setBlur("blur-2xl") }}>Comenzar</Button>
                     </div>
                 </CardBody>
             </Card>
-            <Modal isOpen={isOpen} onOpenChange={() => { onOpenChange(); (answer) ? handleClick() : "" }}>
+            <Modal isOpen={isOpen} onOpenChange={() => { onOpenChange(); setBlur(""); (answer) ? handleClick() : "" }}>
                 <ModalContent className='w-unit-9xl'>
                     {(onClose) => (
                         <>
