d370c24fc0cb7cc53be6217a4e7e9fae01bd62fd
Sergio <principiante.en.programar@gmail.com>
2023-10-26 22:49:46 -0300

    adding adsense

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta name="theme-color" />
+        <meta name="google-adsense-account" content="ca-pub-4379694315841199"></meta>
         <meta name="description" content="Esta pagina es para hacer preguntas random, jugar con tus amigos y divertirse" />
         {/* <link rel="icon" type="image/x-icon" href="../static/favicon.ico" alt="Principiante en Programar" />
         <link rel="apple-touch-icon" href="../static/Principiante-en-Programar.png"></link>*/}
